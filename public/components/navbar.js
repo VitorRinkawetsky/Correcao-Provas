291824aaf32a306a1f6f1aa40984aff03aae7d83
Vitor Rinkawetsky <rinkawetskyv@gmail.com>
2026-08-14 20:28:47 -0300

    feat:setup inicial

---
+++ public/components/navbar.js
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return `
+        <nav>
+            <a href="/">Home</a>
+            <a href="/pages/login.html">Login</a>
+            <a href="/pages/cadastro.html">Cadastro</a>
+        </nav>
+    `;
+}
---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+import { Navbar } from '../components/navbar.js';
+
+const navbar = document.getElementById('navbar');
+
+navbar.innerHTML = Navbar();
