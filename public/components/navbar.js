8862ba5d83264ce91606f523cf624ac2106f080b
Weliton Rodrigues <weliton.rodrigues290@icloud.com>
2026-08-22 21:25:18 -0300

    feat: tela de detalhes da prova com dados mockados

--- public/components/navbar.js
+++ public/components/navbar.js
@@ -4,6 +4,7 @@ export function Navbar() {
             <a href="/">Home</a>
             <a href="/pages/login.html">Login</a>
             <a href="/pages/cadastro.html">Cadastro</a>
+            <a href="/pages/prova-detalhes.html">Detalhes da Prova</a>
         </nav>
     `;
 }
