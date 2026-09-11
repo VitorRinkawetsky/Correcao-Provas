f8ea189ec2db2bfe42be8c55efb662bdb329e61b
Vitor Rinkawetsky <rinkawetskyv@gmail.com>
2026-09-11 20:55:27 -0300

    feat:Criação de fluxo e infra necessário para githubpages.

--- src/client/router/index.js
+++ src/client/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import AppShell from '../components/layout/AppShell.vue';
 import ClassDetailsView from '../views/ClassDetailsView.vue';
@@ -104,7 +104,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
     scrollBehavior: () => ({ top: 0 })
 });
